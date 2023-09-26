e2dab74bd846407f6ddff4b0b3d996832d45390a
Ananda Muhammad Zahir <anandazahir12@gmail.com>
2023-09-26 16:25:27 +0700

    final commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,21 +18,21 @@ export default class Card extends Component {
             nim="21120120140102"
             nama="Maulana Yusuf Arrasyid"
             foto="src\assets\intagram_picture8749873894798.jpg"
-            email="email1@gmail.com"
+            email="maulana2932@gmail.com"
             ig="https://www.instagram.com/maulanaarr/"
           />
           <Square
             nim="21120120120014"
             nama="Sugiyatmi"
-            foto="src\assets\—Pngtree—female avatar vector icon_3725439.png"
-            email="email2@gmail.com"
-            ig="#"
+            foto="src\assets\WhatsApp Image 2023-09-26 at 15.38.31 (1).jpeg"
+            email="sugiyatmi01@gmail.com"
+            ig="https://www.instagram.com/ami_gyt09/"
           />
           <Square
             nim="2112012014014"
             nama="Hafizh Anjar Saputra"
-            foto="src\assets\favpng_user-profile-male-avatar.png"
-            email="email3@gmail.com"
+            foto="src\assets\WhatsApp Image 2023-09-26 at 14.50.15.jpeg"
+            email="hafizh422@gmail.com"
             ig="#"
           />
         </Wrapper>
